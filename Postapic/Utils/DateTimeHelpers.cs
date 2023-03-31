8d29b390cc76aab724a977c6f1c88b61b2bda75b
alvanrahimli <alvan@rahim.li>
2023-03-31 18:55:46 +0400

    FInalizing

---
+++ Postapic/Utils/DateTimeHelpers.cs
@@ -0,0 +1,14 @@
+namespace Postapic.Utils;
+
+public class DateTimeHelpers
+{
+    public static string GetHumanizedTimestamp(DateTime timestamp)
+    {
+        // var diff = DateTime.UtcNow - timestamp;
+        // if (diff.TotalDays > 3) return timestamp.ToString("dd MMMM yyyy");
+        //
+        // var isPast = timestamp < DateTime.UtcNow;
+        //
+        throw new Exception();
+    }
+}
